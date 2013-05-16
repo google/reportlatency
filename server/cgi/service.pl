44e1492c0871e98409790c81d3c12137129db45f
Drake Diedrich <dld@google.com>
2013-05-16 13:01:21 -0700

    Report latency by location for each service.

--- server/cgi/service.pl
+++ server/cgi/service.pl
@@ -38,8 +38,8 @@ No recent reports were found for $name
 EOF
 }
 
-sub service_found($$$$) {
-  my ($q,$service,$meta,$select) = @_;
+sub service_found($$$$$) {
+  my ($q,$service,$meta,$select,$select_location) = @_;
 
   my $rc = $select->execute($service);
 
@@ -56,14 +56,14 @@ sub service_found($$$$) {
 </head>
 <body>
 
-<h1> Latency Report </h1>
-
-<h2>$service $meta->{'date'} </h2>
+<h1> $service $meta->{'date'} Latency Report </h1>
 
 <p align=center>
 <img src="graphs/service/$service.png" width="80%" alt="latency spectrum">
 </img>
 </p>
+
+<h2> All locations, each name </h2>
 
 <table class="alternate">
 <tr>
@@ -130,6 +130,41 @@ EOF
 </tr>
 </table>
 
+<h2> Each location, names aggregated </h2>
+
+<table class="alternate">
+<tr>
+ <th rowspan=2> Location </th>
+ <th colspan=2> Request </th>
+ <th colspan=2> Tab Update </th>
+ <th colspan=2> Navigation </th>
+</tr>
+<tr>
+ <th>Count</th> <th>Latency (ms)</th>
+ <th>Count</th> <th>Latency (ms)</th>
+ <th>Count</th> <th>Latency (ms)</th>
+</tr>
+<hl>
+EOF
+
+  $rc = $select_location->execute($service);
+
+  while ( my $row = $select_location->fetchrow_hashref) {
+    print "  <tr>";
+    print " <td> " . $row->{'remote_addr'} . " </td>";
+    print " <td align=right> " . mynum($row->{'request_count'}) . " </td>";
+    print " <td align=right> " . myround($row->{'request_latency'}) . " </td>";
+    print " <td align=right> " . mynum($row->{'tabupdate_count'}) . " </td>";
+    print " <td align=right> " . myround($row->{'tabupdate_latency'}) . " </td>";
+    print " <td align=right> " . mynum($row->{'navigation_count'}) . " </td>";
+    print ' <td align=right> ' .
+      myround($row->{'navigation_latency'}) . " </a></td>";
+    print "  </tr>\n";
+  }
+
+  $select->finish;
+
+  print <<EOF;
 <p>
 Timespan: $meta->{'min_timestamp'} through $meta->{'max_timestamp'}
 </p>
@@ -176,6 +211,24 @@ sub main {
 		  'ORDER BY name;')
       or die "prepare failed";
 
+  my $select_location_sth =
+    $dbh->prepare('SELECT remote_addr,' .
+                  'sum(tabupdate_count) AS tabupdate_count,' .
+                  'sum(tabupdate_total)/sum(tabupdate_count)' .
+                  ' AS tabupdate_latency,' .
+                  'sum(request_count) AS request_count,' .
+                  'sum(request_total)/sum(request_count)' .
+                  ' AS request_latency,' .
+                  'sum(navigation_count) AS navigation_count,' .
+                  'sum(navigation_total)/sum(navigation_count)' .
+                  ' AS navigation_latency ' .
+                  'FROM report ' .
+                  'WHERE final_name=? AND ' .
+		  "timestamp >= DATETIME('now','-14 days') " .
+                  'GROUP BY remote_addr ' .
+		  'ORDER BY remote_addr;')
+      or die "prepare failed";
+
   my $q = new CGI;
   my $service_name = sanitize_service($q->param('service'));
 
@@ -188,7 +241,7 @@ sub main {
   if (!defined $row) {
     service_not_found($q,$service_name);
   } else {
-    service_found($q,$service_name,$row,$select_sth);
+    service_found($q,$service_name,$row,$select_sth,$select_location_sth);
   }
 
   $dbh->rollback;  # there shouldn't be changes
