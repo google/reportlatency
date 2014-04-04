4dc7c6f2806d23ba1afa6574e8e9f2be28e51b7e
Drake Diedrich <dld@google.com>
2014-04-04 15:51:00 -0700

    in theory, report3 should be equivalent to the report view, just more concise in collapsing rows from the same upload and same service and request name. They are very slow on real data though, and probably cartessian expansions.

--- server/sql/views-sqlite3.sql
+++ server/sql/views-sqlite3.sql
@@ -83,3 +83,101 @@ CREATE VIEW names AS
   SELECT DISTINCT name AS name FROM navigation_request
   UNION
   SELECT DISTINCT name AS name FROM update_request;
+
+CREATE VIEW report2 AS
+    SELECT coalesce(nav.upload,nr.upload) AS upload,
+        coalesce(nav.service,nr.service) AS service,
+        coalesce(nav.name,nr.name) AS name,
+	nav.count AS nav_count, nav.total AS nav_total,
+	nav.high AS nav_high, nav.low AS nav_low,
+	nav.tabclosed AS nav_tabclosed,
+	nr.count AS nreq_count,
+	nr.total AS nreq_total,
+	nr.high AS nreq_high,
+	nr.low AS nreq_low,
+	nr.tabclosed AS nreq_tabclosed,
+	nr.response200 AS nreq_200,
+	nr.response300 AS nreq_300,
+	nr.response400 AS nreq_400,
+	nr.response500 AS nreq_500
+    FROM navigation nav
+    LEFT JOIN navigation_request AS nr
+    ON nav.upload=nr.upload AND nav.service=nr.service AND nav.name=nr.name
+    UNION
+    SELECT coalesce(nav.upload,nr.upload) AS upload,
+        coalesce(nav.service,nr.service) AS service,
+        coalesce(nav.name,nr.name) AS name,
+	nav.count AS nav_count, nav.total AS nav_total,
+	nav.high AS nav_high, nav.low AS nav_low,
+	nav.tabclosed AS nav_tabclosed,
+	nr.count AS nreq_count,
+	nr.total AS nreq_total,
+	nr.high AS nreq_high,
+	nr.low AS nreq_low,
+	nr.tabclosed AS nreq_tabclosed,
+	nr.response200 AS nreq_200,
+	nr.response300 AS nreq_300,
+	nr.response400 AS nreq_400,
+	nr.response500 AS nreq_500
+    FROM navigation_request AS nr
+    LEFT JOIN navigation nav
+    ON nav.upload=nr.upload AND nav.service=nr.service AND nav.name=nr.name;
+
+CREATE VIEW report3 AS
+    SELECT coalesce(r2.upload,ur.upload) AS upload,
+        coalesce(r2.service,ur.service) AS service,
+        coalesce(r2.name,ur.name) AS name,
+	r2.nav_count AS nav_count,
+	r2.nav_total AS nav_total,
+	r2.nav_high AS nav_high,
+	r2.nav_low AS nav_low,
+	r2.nav_tabclosed AS nav_tabclosed,
+	r2.nreq_count AS nreq_count,
+	r2.nreq_total AS nreq_total,
+	r2.nreq_high AS nreq_high,
+	r2.nreq_low AS nreq_low,
+	r2.nreq_tabclosed AS nreq_tabclosed,
+	r2.nreq_200 AS nreq_200,
+	r2.nreq_300 AS nreq_300,
+	r2.nreq_400 AS nreq_400,
+	r2.nreq_500 AS nreq_500,
+	ur.count AS ureq_count,
+	ur.total AS ureq_total,
+	ur.high AS ureq_high,
+	ur.low AS ureq_low,
+	ur.response200 AS ureq_200,
+	ur.response300 AS ureq_300,
+	ur.response400 AS ureq_400,
+	ur.response500 AS ureq_500
+    FROM report2 r2
+    LEFT JOIN update_request AS ur
+    ON r2.upload=ur.upload AND r2.service=ur.service AND r2.name=ur.name
+    UNION
+    SELECT coalesce(r2.upload,ur.upload) AS upload,
+        coalesce(r2.service,ur.service) AS service,
+        coalesce(r2.name,ur.name) AS name,
+	r2.nav_count AS nav_count,
+	r2.nav_total AS nav_total,
+	r2.nav_high AS nav_high,
+	r2.nav_low AS nav_low,
+	r2.nav_tabclosed AS nav_tabclosed,
+	r2.nreq_count AS nreq_count,
+	r2.nreq_total AS nreq_total,
+	r2.nreq_high AS nreq_high,
+	r2.nreq_low AS nreq_low,
+	r2.nreq_tabclosed AS nreq_tabclosed,
+	r2.nreq_200 AS nreq_200,
+	r2.nreq_300 AS nreq_300,
+	r2.nreq_400 AS nreq_400,
+	r2.nreq_500 AS nreq_500,
+	ur.count AS ureq_count,
+        ur.total AS ureq_total,
+	ur.high AS ureq_high,
+	ur.low AS ureq_low,
+	ur.response200 AS ureq_200,
+	ur.response300 AS ureq_300,
+	ur.response400 AS ureq_400,
+	ur.response500 AS ureq_500
+    FROM update_request ur
+    LEFT JOIN report2 AS r2
+    ON r2.upload=ur.upload AND r2.service=ur.service AND r2.name=ur.name;
