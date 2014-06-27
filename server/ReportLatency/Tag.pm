2e9dd4cb8631c95800e5dc8b98f7ac894112105e
Drake Diedrich <dld@google.com>
2014-06-27 15:37:02 -0700

    Add a Tag query object for the tag reports.

---
+++ server/ReportLatency/Tag.pm
@@ -0,0 +1,206 @@
+# Copyright 2014 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+package ReportLatency::Tag;
+use ReportLatency::Base;
+use Carp qw( croak );
+@ISA = ("ReportLatency::Base");
+
+use strict;
+use vars qw($VERSION);
+
+$VERSION     = 0.1;
+
+sub new {
+  my $class = shift;
+  my $self  = bless {}, $class;
+  $self->{store} = shift;
+  my $begin = $self->{begin} = shift;
+  my $end = $self->{end} = shift;
+  $self->{tag} = shift;
+
+  $self->{store}->create_current_temp_table($begin,$end);
+
+  return $self;
+}
+
+sub title {
+  my $self=shift;
+  return $self->tag;
+}
+
+sub name_title {  return "Service"; }
+sub count_title { return "Depend"; }
+sub meta_count_title { return "Services"; }
+
+sub execute {
+  my ($self,$sth) = @_;
+  return $sth->execute($self->{tag}) or croak $sth->errstr;
+}
+
+sub latency_select {
+  my ($self,$latency) = @_;
+  my $store = $self->{store};
+  my $ts = $store->unix_timestamp('u.timestamp');
+  my $positive = $store->is_positive('n.count');
+  return <<EOS;
+SELECT 
+  $ts AS timestamp,
+  n.count AS count,
+  n.high AS high,
+  n.low AS low,
+  n.total AS total 
+FROM current u, $latency n, tag t
+WHERE t.tag=? AND t.service=n.service AND n.upload=u.id AND $positive;
+EOS
+}
+
+sub latency_histogram {
+  my ($self,$latency) = @_;
+  return <<EOS;
+SELECT utimestamp AS timestamp,'closed' AS measure,tabclosed AS amount 
+FROM current AS u
+INNER JOIN $latency AS n ON n.upload=u.id
+LEFT OUTER JOIN tag AS t ON t.service=n.service
+WHERE t.tag=? AND tabclosed>0
+UNION
+SELECT utimestamp AS timestamp,'100ms' AS measure,m100 AS amount 
+FROM current AS u
+INNER JOIN $latency AS n ON n.upload=u.id
+LEFT OUTER JOIN tag AS t ON t.service=n.service
+WHERE t.tag=? AND m100>0
+UNION
+SELECT utimestamp AS timestamp,'500ms' AS measure,m500 AS amount 
+FROM current AS u
+INNER JOIN $latency AS n ON n.upload=u.id
+LEFT OUTER JOIN tag AS t ON t.service=n.service
+WHERE t.tag=? AND m500>0
+UNION
+SELECT utimestamp AS timestamp,'1s' AS measure,m1000 AS amount 
+FROM current AS u
+INNER JOIN $latency AS n ON n.upload=u.id
+LEFT OUTER JOIN tag AS t ON t.service=n.service
+WHERE t.tag=? AND m1000>0
+UNION
+SELECT utimestamp AS timestamp,'2s' AS measure,m2000 AS amount 
+FROM current AS u
+INNER JOIN $latency AS n ON n.upload=u.id
+LEFT OUTER JOIN tag AS t ON t.service=n.service
+WHERE t.tag=? AND m2000>0
+UNION
+SELECT utimestamp AS timestamp,'4s' AS measure,m4000 AS amount 
+FROM current AS u
+INNER JOIN $latency AS n ON n.upload=u.id
+LEFT OUTER JOIN tag AS t ON t.service=n.service
+WHERE t.tag=? AND m4000>0
+UNION
+SELECT utimestamp AS timestamp, '10s' AS measure,m10000 AS amount 
+FROM current AS u
+INNER JOIN $latency AS n ON n.upload=u.id
+LEFT OUTER JOIN tag AS t ON t.service=n.service
+WHERE t.tag=? AND m10000>0
+UNION
+SELECT utimestamp AS timestamp,'long' AS measure,
+COALESCE(count,0)-COALESCE(m100,0)-COALESCE(m500,0)-COALESCE(m1000,0)-COALESCE(m2000,0)-COALESCE(m4000,0)-COALESCE(m10000,0)-COALESCE(tabclosed,0) AS amount 
+FROM current AS u
+INNER JOIN $latency AS n ON n.upload=u.id
+LEFT OUTER JOIN tag AS t ON t.service=n.service
+WHERE t.tag=? AND amount>0
+EOS
+}
+
+sub nav_latency_histogram {
+  my ($self) = @_;
+
+  my $dbh = $self->{store}->{dbh};
+  my $sth = $dbh->prepare( $self->latency_histogram('navigation'))
+   or die $!;
+  my $tag = $self->{tag};
+  $sth->execute($tag,$tag,$tag,$tag,$tag,$tag,$tag,$tag);
+  return $sth;
+}
+
+sub nreq_latency_histogram {
+  my ($self) = @_;
+
+  my $dbh = $self->{store}->{dbh};
+  my $sth = $dbh->prepare( $self->latency_histogram('navigation_request') )
+    or die $!;
+  my $tag = $self->{tag};
+  $sth->execute($tag,$tag,$tag,$tag,$tag,$tag,$tag,$tag);
+  return $sth;
+}
+
+sub ureq_latency_histogram {
+  my ($self) = @_;
+
+  my $dbh = $self->{store}->{dbh};
+  my $sth = $dbh->prepare( $self->latency_histogram('update_request') )
+    or die $!;
+  my $tag = $self->{tag};
+  $sth->execute($tag,$tag,$tag,$tag,$tag,$tag,$tag,$tag);
+  return $sth;
+}
+
+sub meta_select {
+  my ($self) = @_;
+  my $store = $self->{store};
+  my $fields = $store->common_aggregate_fields();
+  my $st = <<EOS;
+SELECT t.tag AS tag,
+min(min_timestamp) AS min_timestamp,
+max(max_timestamp) AS max_timestamp,
+count(distinct r.service) AS services,
+$fields
+FROM service_report AS r,tag t
+WHERE t.tag=? AND t.service = r.service
+EOS
+  return $st;
+}
+
+sub tag_select {
+  my ($self) = @_;
+
+  my $store = $self->{store};
+  my $fields = $store->common_aggregate_fields();
+  return <<EOS;
+SELECT r.service as tag,
+count(distinct r.name) AS services,
+$fields
+FROM service_report r, tag t
+WHERE t.tag=? AND r.service = t.service
+GROUP BY r.service
+ORDER BY r.service
+;
+EOS
+}
+
+
+sub location_select {
+  my ($self) = @_;
+
+  my $store = $self->{store};
+  my $fields = $store->common_aggregate_fields();
+  return <<EOS;
+SELECT location,
+count(distinct r.service) AS services,
+$fields
+FROM service_report r, tag t
+WHERE t.tag=? AND r.service = t.service
+GROUP BY location
+ORDER BY location;
+EOS
+}
+
+1;
---
+++ server/ReportLatency/t/Tag.t
@@ -0,0 +1,121 @@
+#!/usr/bin/perl -w
+#
+# Test ReportLatency::Tag.pm
+#
+# Copyright 2014 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+use strict;
+use DBI;
+use File::Temp qw(tempfile tempdir);
+use Test::More tests => 34;
+use Data::Dumper;
+
+BEGIN { use lib '..'; }
+
+use_ok( 'ReportLatency::Tag' );
+use ReportLatency::Store;
+
+
+my $dir = tempdir(CLEANUP => 1);
+my $dbfile = "$dir/latency.sqlite3";
+{
+  open(my $sqlite3,"|-",'sqlite3',$dbfile) or die $!;
+  open(my $sql,'<','../sql/sqlite3.sql') or die $!;
+  while (my $line = $sql->getline) {
+    print $sqlite3 $line;
+  }
+  print $sqlite3 <<EOD;
+INSERT INTO upload(location) VALUES("1.2.3.0");
+INSERT INTO navigation(upload,name,service,count,total,low,high,m10000,response200)
+  VALUES(1,'mail.google.com','mail.google.com',2,22038,4038,18000,1,2);
+INSERT INTO navigation_request(upload,name,service,count,total,low,high)
+  VALUES(1,'mail.google.com','mail.google.com',3,2100,600,800);
+INSERT INTO upload(location) VALUES("1.2.3.0");
+INSERT INTO update_request(upload,name,service,count,total,low,high)
+  VALUES(2,'mail.google.com','mail.google.com',10,2220,100,300);
+INSERT INTO upload(location) VALUES("1.2.3.0");
+INSERT INTO update_request(upload,name,service,count,total)
+  VALUES(3,'news.google.com','news.google.com',10,3330);
+INSERT INTO tag(tag,service) VALUES('Mail','mail.google.com');
+EOD
+
+  close($sql);
+  ok(close($sqlite3),'latency schema');
+}
+
+my $store = new ReportLatency::Store(dsn => "dbi:SQLite:dbname=$dbfile");
+
+my $b = time-300;
+my $e = time+300;
+my $begin = $store->db_timestamp($b);
+my $end = $store->db_timestamp($e);
+my $qobj = new ReportLatency::Tag($store,$begin,$end,'Mail');
+isa_ok($qobj, 'ReportLatency::Tag');
+
+
+my $sth = $qobj->nav_latencies();
+my $row = $sth->fetchrow_hashref;
+is($row->{count}, 2, 'total nav latency count');
+is($row->{total}, 22038, 'total');
+is($row->{low}, 4038, 'low');
+is($row->{high}, 18000, 'high');
+cmp_ok($row->{timestamp}, '<=', time, 'timestamp <= now');
+cmp_ok($row->{timestamp}, '>', time-300, 'timestamp > now-300');
+$row = $sth->fetchrow_hashref;
+is($row, undef, 'last nav latency row');
+
+$sth = $qobj->nav_latency_histogram();
+isa_ok($sth, 'DBI::st');
+$row = $sth->fetchrow_hashref;
+ok($row, "got a row from nav_latency_histogram");
+is($row->{amount}, 1, '1 navigation');
+is($row->{measure}, '10s', ' in 10s bin');
+cmp_ok($row->{timestamp}, '<=', time, 'timestamp <= now');
+cmp_ok($row->{timestamp}, '>', time-300, 'timestamp > now-300');
+$row = $sth->fetchrow_hashref;
+is($row->{amount}, 1, '1 navigation');
+is($row->{measure}, 'long', ' in long bin');
+$row = $sth->fetchrow_hashref;
+is($row, undef, 'last nav_latency_histogram row');
+
+$sth = $qobj->nreq_latencies();
+$row = $sth->fetchrow_hashref;
+is($row->{count}, 3, 'total nreq count');
+is($row->{total}, 2100, 'total');
+is($row->{low}, 600, 'low');
+is($row->{high}, 800, 'high');
+cmp_ok($row->{timestamp}, '<=', time, 'timestamp <= now');
+cmp_ok($row->{timestamp}, '>', time-300, 'timestamp > now-300');
+$row = $sth->fetchrow_hashref;
+is($row, undef, 'last total nreq latency row');
+
+$sth = $qobj->ureq_latencies();
+my ($count,$total,$rows);
+while (my $row = $sth->fetchrow_hashref) {
+  $count += $row->{count};
+  $total += $row->{total};
+  $rows++;
+  cmp_ok($row->{timestamp}, '<=', time, 'timestamp <= now');
+  cmp_ok($row->{timestamp}, '>', time-300, 'timestamp > now-300');
+}
+is($rows, 1, '1 total ureq latency rows');
+is($count, 10, '10 total ureq count');
+is($total, 2220, '2220 ms total ureq latency');
+
+
+$row = $qobj->meta();
+is($row->{nav_count}, 2, '2 meta nav_count');
+is($row->{nreq_count}, 3, '3 meta nreq_count');
+is($row->{ureq_count}, 10, '10 meta ureq_count');
