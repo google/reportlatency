b8e157c8d69d48aad7a1d56eabd5de7578a582f0
Drake Diedrich <dld@google.com>
2014-08-12 13:44:35 -0700

    Replace final location code in generate-static-content.pl with location.pl

--- server/debian/reportlatency.cron.d
+++ server/debian/reportlatency.cron.d
@@ -2,7 +2,7 @@
 # Regular cron jobs for the google-reportlatency-service package
 #
 #
-# Copyright 2013 Google Inc. All Rights Reserved.
+# Copyright 2013,2014 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,8 +19,7 @@
 #
 PATH=/usr/local/scripts:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 MAILTO=""
-1,31 * * * * www-data [ -x /usr/share/reportlatency/generate-static-content.pl ] && cd /var/lib/reportlatency/www && /usr/share/reportlatency/generate-static-content.pl >/dev/null 2>&1
-25 4 * * * www-data [ -x /usr/share/reportlatency/generate-static-content.pl ] && cd /var/lib/reportlatency/www && /usr/share/reportlatency/generate-static-content.pl -all >/dev/null 2>&1
+1,31 * * * * www-data [ -x /usr/share/reportlatency/location.pl ] && cd /var/lib/reportlatency/www && /usr/share/reportlatency/location.pl >/dev/null 2>&1
 11,41 * * * * www-data [ -x /usr/share/reportlatency/untagged.pl ] && cd /var/lib/reportlatency/www && /usr/share/reportlatency/untagged.pl >/dev/null 2>&1
 21,51 * * * * www-data [ -x /usr/share/reportlatency/summary.pl ] && cd /var/lib/reportlatency/www && /usr/share/reportlatency/summary.pl >/dev/null 2>&1
 01 4 * * * www-data cat /etc/reportlatency/tag.d/*.sql /var/lib/reportlatency/tag.d/*.sql > /var/lib/reportlatency/data/tag.sql ; /usr/bin/sqlite3 /var/lib/reportlatency/data/latency.sqlite3 </usr/share/reportlatency/update-tag.sql >/dev/null 2>&1
